c6a2a5da50db41c0b825efa2f9234376ce610367
nickSwany <Thief.leroy@yandex.ru>
2024-05-12 01:10:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tetris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val linerLayout: LinearLayout = LinearLayout(this)
+    }
+}
